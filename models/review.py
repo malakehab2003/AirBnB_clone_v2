ab4508ba971701c56339f41844d24c1af5dff515
DJ-Man-2099 <davidmesak@hotmail.com>
2024-01-16 20:41:01 +0200

    Task 9

--- models/review.py
+++ models/review.py
@@ -9,7 +9,7 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     place = relationship("Place")
-    place_id = Column(String(1024), ForeignKey("places.id"), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user = relationship("User")
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    text = Column(String(60), nullable=False)
+    text = Column(String(1024), nullable=False)
