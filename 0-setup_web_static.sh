c88c2b5fab1ac64def0e8da5b51260c24ad34237
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-02 01:37:38 +0200

    Task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # sets up web servers for the deployment of web_static
 # Create directories recursively if they don't exist
-apt-get update
-apt-get install -y nginx
+if [ ! -x /usr/sbin/nginx ]
+then
+apt-get -y update
+apt-get -y install nginx
+fi
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -14,7 +17,7 @@ echo "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 chown -R ubuntu:ubuntu /data/
-sudo chmod -R 755 /data/
+chmod -R 755 /data/
 sed -i -e "0,/    }/ s#    }# }\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n#" /etc/nginx/sites-available/default
 nginx -t
 nginx -s reload
