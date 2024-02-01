1e1249339031fa84fc10e8758000cd6baaaad98d
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-02 01:04:56 +0200

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,4 +9,5 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 echo "this is the simple content" > /data/web_static/releases/test/index.html
 chown -R ubuntu:ubuntu /data/
 sed -i -e "0,/    }/ s#    }# }\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n#" /etc/nginx/sites-available/default
+nginx -t
 nginx -s reload
