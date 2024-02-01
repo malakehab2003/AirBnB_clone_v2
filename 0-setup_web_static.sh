a010bebd8e1d935bb93f72b66b153e3c27c95df4
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-02 01:11:39 +0200

    Task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,13 @@ apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-echo "this is the simple content" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 chown -R ubuntu:ubuntu /data/
 sed -i -e "0,/    }/ s#    }# }\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n#" /etc/nginx/sites-available/default
 nginx -t
