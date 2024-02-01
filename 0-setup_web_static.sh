01169408f122fac6e2693dc354d52c9774d459fd
DJ-Man-2099 <davidmesak@hotmail.com>
2024-02-02 00:48:50 +0200

    Fix bug in login functionality

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# sets up web servers for the deployment of web_static
+# Create directories recursively if they don't exist
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo "this is the simple content" > /data/web_static/releases/test/index.html
+chown -R ubuntu:ubuntu /data/
+sed -i -e "0,/    }/ s#    }# }\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n#" /etc/nginx/sites-available/default
+nginx -s reload
