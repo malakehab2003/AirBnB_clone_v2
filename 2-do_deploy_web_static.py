0995e1406c5900ac05a30eb6bb7efca5c1a8c282
malakehab2003 <lokaehab2003@gmail.com>
2024-02-04 23:04:31 +0200

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -34,22 +34,34 @@ def do_pack():
 
 def do_deploy(archive_path):
     """Distributes an archive to web servers."""
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
-
-        print("New version deployed!")
-        return True
-
-    return False
+    if os.path.exists(archive_path) is False:
+        return (False)
+    result = put(archive_path, "/tmp/")
+    if result.failed:
+        return False
+    name = archive_path.split("/")[-1].split(".")[0]
+    result = run(
+        f"mkdir -p /data/web_static/releases/{name}")
+    if result.failed:
+        return False
+    result = run(
+        f"tar -xvf /tmp/{name}.tgz -C /data/web_static/releases/{name}")
+    if result.failed:
+        return False
+    result = run(
+        f"mv /data/web_static/releases/{name}/web_static/* \
+          /data/web_static/releases/{name}")
+    if result.failed:
+        return False
+    result = run(f"rm /tmp/{name}.tgz")
+    if result.failed:
+        return False
+    result = run(
+        f"rm /data/web_static/current")
+    if result.failed:
+        return False
+    result = run(
+        f"ln -sf /data/web_static/releases/{name} /data/web_static/current")
+    if result.failed:
+        return False
+    return True
