d8205f42fe1b0809f431c1d3d41c1fb92b0a8c56
malakehab2003 <lokaehab2003@gmail.com>
2024-02-05 00:42:35 +0200

    task 2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -29,7 +29,28 @@ sudo chown -R ubuntu:ubuntu /data/
 sudo chmod -R 755 /data/
 
 # Check if alias already exists, replace it; otherwise, add the alias
-sudo sed -i '48 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
