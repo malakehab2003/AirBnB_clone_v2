15c5757fd52a3564120c0f55d76bf6e3f10e01db
malakehab2003 <lokaehab2003@gmail.com>
2024-02-17 22:31:59 +0200

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" start a web flas application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """say hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """say HBNB"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    """if module run as main"""
+    app.run(host='0.0.0.0', port=5000)
