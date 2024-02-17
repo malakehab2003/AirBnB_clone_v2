64eea9f00c7f5be0e541b5da8f68f617fdce36c1
malakehab2003 <lokaehab2003@gmail.com>
2024-02-17 22:06:54 +0200

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """say hello"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main":
     """if module run as main"""
